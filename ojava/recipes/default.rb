d6a2d04a4804e8ad6f7287c0fd571b5c5b2b74ba
samirjha123 <skjecnit@gmail.com>
2014-01-06 17:27:38 +0530

    Update default.rb

--- ojava/recipes/default.rb
+++ ojava/recipes/default.rb
@@ -2,9 +2,16 @@ package 'apache2' do
   case node[:platform]
   when 'centos','redhat','fedora','amazon'
     package_name 'httpd'
-    Chef::Log.debug("samirjj1")
   when 'debian','ubuntu'
     package_name 'apache2'
   end
-  action :install
+    action :install
+end
+
+java_ark "jdk" do
+    url 'http://download.oracle.com/otn-pub/java/jdk/6u29-b11/jdk-6u29-linux-x64.bin'
+    checksum  'a8603fa62045ce2164b26f7c04859cd548ffe0e33bfc979d9fa73df42e3b3365'
+    app_home '/usr/local/java/default'
+    bin_cmds ["java", "javac"]
+    action :install
 end
