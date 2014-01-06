a23b5f4e594dc6baa6f8ab70020670a63b478ef8
samirjha123 <skjecnit@gmail.com>
2014-01-06 10:41:05 +0530

    Delete default.rb

--- default.rb
+++
@@ -1,9 +0,0 @@
-package 'apache2' do
-  case node[:platform]
-  when 'centos','redhat','fedora','amazon'
-    package_name 'httpd'
-  when 'debian','ubuntu'
-    package_name 'apache2'
-  end
-  action :install
-end
