cb6dd99ba90ec78f5edcd33ed42829c6a2da4b25
samirjha123 <skjecnit@gmail.com>
2014-01-08 08:57:49 +0530

    Delete ppa.rb

--- ojava/recipes/ppa.rb
+++
@@ -1,8 +0,0 @@
-apt_repository "oracle-java-6" do
-  uri "http://ppa.launchpad.net/webupd8team/java/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "EEA14886"
-  action :add
-end
