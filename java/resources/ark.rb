05147c341c49077dfbb2d8539e964281e59936df
samirjha123 <skjecnit@gmail.com>
2014-01-09 13:01:28 +0530

    Update ark.rb

--- java/resources/ark.rb
+++ java/resources/ark.rb
@@ -7,9 +7,9 @@ attribute :app_home, :kind_of => String, :default => nil
 attribute :app_home_mode, :kind_of => Integer, :default => 0755
 attribute :bin_cmds, :kind_of => Array, :default => nil
 attribute :owner, :default => "root"
-attribute :accept_oracle_download_terms,  :equal_to => [true, false], :default => true
-attribute :username, :default => samirkumarjha007@gmail.com
-attribute :password, :default => samirJHA123
+# attribute :accept_oracle_download_terms,  :equal_to => [true, false], :default => true
+# attribute :username, :default => samirkumarjha007@gmail.com
+# attribute :password, :default => samirJHA123
 attribute :default, :equal_to => [true, false], :default => true
 
 # we have to set default for the supports attribute
