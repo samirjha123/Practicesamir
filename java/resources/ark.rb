9ff68e52e70ee797d5fe162d62b8bf54357e6d62
samirjha123 <skjecnit@gmail.com>
2014-01-09 08:58:08 +0530

    Update ark.rb

--- java/resources/ark.rb
+++ java/resources/ark.rb
@@ -7,6 +7,7 @@ attribute :app_home, :kind_of => String, :default => nil
 attribute :app_home_mode, :kind_of => Integer, :default => 0755
 attribute :bin_cmds, :kind_of => Array, :default => nil
 attribute :owner, :default => "root"
+attribute :accept_oracle_download_terms,  :equal_to => [true, false], :default => true
 attribute :default, :equal_to => [true, false], :default => true
 
 # we have to set default for the supports attribute
