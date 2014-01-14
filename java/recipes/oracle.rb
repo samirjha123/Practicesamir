a2a7ce00884c9339fa48006194e10a8a7ea856ff
samirjha123 <skjecnit@gmail.com>
2014-01-14 09:30:31 +0530

    Update oracle.rb

--- java/recipes/oracle.rb
+++ java/recipes/oracle.rb
@@ -1,7 +1,6 @@
 java_home = node['java']["java_home"]
 arch = node['java']['arch']
 jdk_version = node['java']['jdk_version']
-java_home = node['java']['java_home']
 
 #convert version number to a string if it isn't already
 if jdk_version.instance_of? Fixnum
@@ -12,30 +11,24 @@ case jdk_version
 when "6"
   tarball_url = node['java']['jdk']['6'][arch]['url']
   tarball_checksum = node['java']['jdk']['6'][arch]['checksum']
-  accept_oracle_download_terms = node['java']['oracle']['accept_oracle_download_terms']
-  # tarball_username = node['java']['oracle']['username']
-  # tarball_password = node['java']['oracle']['password']
-  
+  bin_cmds = node['java']['jdk']['6']['bin_cmds']
 when "7"
   tarball_url = node['java']['jdk']['7'][arch]['url']
   tarball_checksum = node['java']['jdk']['7'][arch]['checksum']
- # tarball_accept_oracle_download_terms = node['java']['oracle']['accept_oracle_download_terms']
+  bin_cmds = node['java']['jdk']['7']['bin_cmds']
 end
 
-ruby_block  "set-env-java-home" do
-  block do
-    ENV["JAVA_HOME"] = java_home
-  end
+if tarball_url =~ /example.com/
+  Chef::Application.fatal!("You must change the download link to your private repository. You can no longer download java directly from http://download.oracle.com without a web broswer")
+end
 
+include_recipe "java::set_java_home"
 
 java_ark "jdk" do
- url tarball_url
- checksum tarball_checksum
- app_home java_home
- bin_cmds ["java"]
- action :install
+  url tarball_url
+  checksum tarball_checksum
+  app_home java_home
+  bin_cmds bin_cmds
+  alternatives_priority 1062
+  action :install
 end
-#bash "jdk" do
-  #code "wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "https://edelivery.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-linux-x64.tar.gz""
-  #action :install
-#end
