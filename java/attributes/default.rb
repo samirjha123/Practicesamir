f36581035443d2adebe548e40fe479b58c821e7c
samirjha123 <skjecnit@gmail.com>
2014-01-16 15:23:11 +0530

    Update default.rb

--- java/attributes/default.rb
+++ java/attributes/default.rb
@@ -45,7 +45,7 @@ end
 
 # if you change this to true, you can download directly from Oracle
 else
-default['java']['oracle']['accept_oracle_download_terms'] = false
+default['java']['oracle']['accept_oracle_download_terms'] = true
 
 # direct download paths for oracle, you have been warned!
 
