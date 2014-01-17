3f79efd2dc9816a4a53e6acb73f14523c7941bef
samirjha123 <skjecnit@gmail.com>
2014-01-17 16:40:56 +0530

    Create default.rb

---
+++ deployment/providers/default.rb
@@ -0,0 +1,15 @@
+ def initialize(new_resource, run_context)
+    super(new_resource, run_context)
+    @s3 = S3Deploy.new(new_resource, run_context)
+end
+
+# Override
+def load_current_resource
+  @s3.load_current_resource 
+end
+
+# Delegate everything else
+def method_missing(name, *args)
+  @s3.send(name, *args)
+end
+ 
