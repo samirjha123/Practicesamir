efa971edcd7ff2f19e7044d29701562574c0fce0
samirjha123 <skjecnit@gmail.com>
2014-01-17 08:37:53 +0530

    Delete base.rb

--- java/roles/base.rb
+++
@@ -1,9 +0,0 @@
-default_attributes(
-  :java => {
-     :oracle => {
-       "accept_oracle_download_terms" => true
-       #"username" => samirkumarjha007@gmail.com
-       #"password" => samirJHA123
-     }
-   }
-)
