f55f04fd5b2a6e0c1a6cbcc21c52519c9fbf55c6
samirjha123 <skjecnit@gmail.com>
2014-01-20 11:11:29 +0530

    Update default.rb

--- mvn_s3_deploy/recipes/default.rb
+++ mvn_s3_deploy/recipes/default.rb
@@ -11,7 +11,8 @@
 
 # Build the URL based on Maven2 Repository Layout
 props = node["mvn_s3_deploy"];
-file_name = props["artifactId"] + "-" + props["version"] + ".war"
+file_name = props["artifactId"] + ".war"
+#file_name = props["artifactId"] + "-" + props["version"] + ".war"
 full_url = "s3://#{props['bucket_name']}/#{file_name}"
 #full_url = "s3://#{props['bucket_name']}/release/#{props['groupId'].gsub('.','/')}/#{props['artifactId']}/#{props['version']}/#{file_name}"
 file_path = node["tomcat"]["webapp_dir"] + "/"  + (props["war_name"] || file_name)
