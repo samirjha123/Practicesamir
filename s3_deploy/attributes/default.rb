3ee0c7c218935efe602efcffb0bf7d97dac0966b
samirjha123 <skjecnit@gmail.com>
2014-01-23 15:51:44 +0530

    Update default.rb

--- s3_deploy/attributes/default.rb
+++ s3_deploy/attributes/default.rb
@@ -1,7 +1,7 @@
 efault[:mvn_s3_deploy][:access_key_id] = "YOUR_KEY_HERE"
 default[:mvn_s3_deploy][:secret_access_key] = "YOUR_SECRET_KEY_HERE"
-default[:mvn_s3_deploy][:bucket_name] = "BUCKET_NAME_HERE"
-default[:mvn_s3_deploy][:artifactId] = "ARTIFACT_ID" # The maven artifactId for your war application
-default[:mvn_s3_deploy][:groupId] = "GROUP_ID" # The maven groupId for your war application
+default[:mvn_s3_deploy][:bucket_name] = "elasticbeanstalk-ap-northeast-1-724566739352"
+default[:mvn_s3_deploy][:artifactId] = "samir" # The maven artifactId for your war application
+default[:mvn_s3_deploy][:groupId] = "samir" # The maven groupId for your war application
 default[:mvn_s3_deploy][:version] = "0.0.1-SNAPSHOT" # The version number NOTE: in the maven distribution sections, unsure uniqueVersion is false!
-default[:mvn_s3_deploy][:war_name] = "WAR_OR_CONTEXT_NAME.war" # The name of the file as it will reside in the webapps directory and by default, the context name for the application
+default[:mvn_s3_deploy][:war_name] = "samir.war" # The name of the file as it will reside in the webapps directory and by default, the context name for the application
