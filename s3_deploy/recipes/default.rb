14c0510b705b4c0e11f36fd02c1b9bc86bc60332
samirjha123 <skjecnit@gmail.com>
2014-01-23 16:55:18 +0530

    Update default.rb

--- s3_deploy/recipes/default.rb
+++ s3_deploy/recipes/default.rb
@@ -1,8 +1,26 @@
 # Make sure gem is installed
-gem_package "aws-s3" do
-  
-action :install
+case node.platform
+when "debian", "ubuntu"
+  #include_recipe "apt"
+  packages = ['libxml2-dev','libxslt-dev']
+  packages.each do |pkg|
+    package pkg do
+      action :install
+    end
+  end
+when "centos", "redhat", "fedora"
+  packages = ['libxml2-devel','libxslt-devel']
+  packages.each do |pkg|
+    package pkg do
+      action :install
+    end
+  end
+else
+  Chef::Log.error("Can't install on #{node.platform} yet.")
+end
 
+gem_package "aws-s3" do
+  action :install
 end
 
 require 'rubygems'
