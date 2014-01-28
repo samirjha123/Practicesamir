c6565c4ed6bdce022400e7d4e07c507c9541ffe9
samirjha123 <skjecnit@gmail.com>
2014-01-28 17:20:59 +0530

    Update default.rb

--- tomcat/recipes/default.rb
+++ tomcat/recipes/default.rb
@@ -43,9 +43,9 @@ end
 service "tomcat" do
   service_name "tomcat#{node["tomcat"]["base_version"]}"
   case node["platform"]
-  when "centos","redhat","fedora"
+  when "centos","redhat","fedora","amazon"
     supports :restart => true, :status => true
-  when "debian","ubuntu","amazon"
+  when "debian","ubuntu"
     supports :restart => true, :reload => false, :status => true
   end
   action [:enable, :start]
@@ -146,5 +146,5 @@ end
 
 execute "service iptables stop" do
   user "root"
-  command "ufw disable"
+  command "service iptables stop"
 end
