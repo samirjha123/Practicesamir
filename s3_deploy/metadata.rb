b59271d6c000b7a9fe6b278e06d7a1c36de43925
samirjha123 <skjecnit@gmail.com>
2014-01-29 11:31:32 +0530

    Update metadata.rb

--- s3_deploy/metadata.rb
+++ s3_deploy/metadata.rb
@@ -4,7 +4,6 @@ license          "Apache 2.0"
 description      "Deploys a war stored in a private S3 bucket"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
-depends          "tomcat"
 
 %w{ s3_file }.each do |cookbook|
   depends cookbook
